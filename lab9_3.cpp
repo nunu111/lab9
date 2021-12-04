e61f99c691c2a32db7572f72109437fe90de907a
nunu111 <nunu111za@hotmail.com>
2021-12-04 12:50:55 +0700

    lab9_3 completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -9,3 +9,55 @@
 "ONE-NIGHT-STAND"
 "MARRIED"
 */
+#include <iostream>
+#include <string>
+using namespace std;
+
+double age,height,money;
+string status;
+
+int main ()
+{
+
+
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age <= 20)
+    {
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 160)
+        {
+            status = "UNFRIEND";
+        }
+        else if (height < 175)
+        {
+             status = "FRIEND";
+        }
+        else
+        {
+            cout << "Enter your property: ";
+            cin >> money;
+            if (money > 69e6)
+            {
+               status = "MARRIED";
+            }
+            else
+                 status = "ONE-NIGHT-STAND";
+        }
+    }
+    else if (age < 30)
+    {
+        cout << "Enter your property: ";
+        cin >> money;
+        if (money > 10e6)
+        {
+            status = "BEST FRIEND";
+        }
+        else
+            status = "UNFRIEND";
+    }
+    else
+        status = "UNFRIEND";
+    cout << "Your status = " << status;
+}
